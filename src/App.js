6cb48579dd6cbd1eb2e72cc3e8953d00aef43cd2
Matt Burnett <mb@mattburnettmusic.com>
2021-09-23 23:29:10 +0200

    frm youtube tutorial https://youtu.be/rqw3OftE5sA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// this is more voice control than AI, but whatever...
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js'
+
+// keys should be in .env file
+const alanKey = '1adc1da911479573a37317f834ae6d552e956eca572e1d8b807a3e2338fdd0dc/stage'
+// const newsApiKey = '478fe77828c9461c82f48f6b8d2a69e7'
+
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([])
+    const [activeArticle, setActiveArticle] = useState(-1)
+
+    const classes = useStyles()
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles)
+                    setActiveArticle(-1)
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+                } else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+          
+                    if (parsedNumber > articles.length) {
+                      alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                      window.open(article.url, '_blank');
+                      alanBtn().playText('Opening...');
+                    } else {
+                      alanBtn().playText('Please try that again...');
+                    }
+                }
+            }
+        })
+    }, [])
+
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                {/* TODO: image appears as broken */}
+                {/* <img src="../assets/alan-ai-logo.png" className={classes.alanLogo} alt="alan logo"/> */}
+                <h1>Alan AI Voice Control Demo</h1>
+            </div>
+            <NewsCards articles={ newsArticles } activeArticle={ activeArticle }/>
+        </div>
+    )
 }
 
 export default App;
